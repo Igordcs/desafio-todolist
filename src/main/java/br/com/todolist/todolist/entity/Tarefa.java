8f49296ff845a065fd9600867a2d7804f40ce34a
igordcs <igorcostamdt@gmail.com>
2024-06-06 11:39:21 -0300

    Relacionamento entre tarefa e membro

--- src/main/java/br/com/todolist/todolist/entity/Tarefa.java
+++ src/main/java/br/com/todolist/todolist/entity/Tarefa.java
@@ -12,32 +12,39 @@ public class Tarefa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "membro_id", nullable = false)
+    private Membro criador;
+
     @Column(nullable = false)
     @NotNull
     @Size(min = 5, max = 50, message = "O nome deve ter entre 5 e 50 caracteres!")
     private String nome;
 
-    @Size(max = 140, message = "A descricao deve ter até 140 caracteres!")
+    @Size(max = 140, message = "A descricao deve ter ate 140 caracteres!")
     private String descricao;
 
     @Column(nullable = false)
     @NotNull
     private boolean finalizada;
-
+    
     private Date dataTermino;
 
     @Column(nullable = false)
     private Prioridade prioridade = Prioridade.BAIXA;
 
-    public Tarefa(){}
-
-    public Tarefa(String nome, String descricao, boolean finalizada, Date dataTermino, Prioridade prioridade) {
+    public Tarefa(String nome, String descricao, Membro criador, boolean finalizada, Date dataTermino, Prioridade prioridade) {
         super();
         this.nome = nome;
         this.descricao = descricao;
+        this.criador = criador;
         this.finalizada = finalizada;
         this.dataTermino = dataTermino;
         this.prioridade = prioridade;
+    }
+
+    public Tarefa(){
+        super();
     }
 
     public Long getId() {
@@ -58,6 +65,14 @@ public class Tarefa {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public Membro getCriador() {
+        return this.criador;
+    }
+
+    public void setCriador(Membro criador) {
+        this.criador = criador;
     }
 
     public boolean getFinalizada() {
